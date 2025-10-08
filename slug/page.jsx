20c842022470040cded50d0e5b32cf93f1eae638
Nikhilesh Nanda <nnikhilesh17@gmail.com>
2025-10-08 19:42:39 +0530

    Add files via upload

---
+++ slug/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./random.css"
+const page = () => {
+  //if someone try to access route that is not defined
+  return (
+    <div>
+        <title>ERROR 404</title>
+        <h1>404 NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default page;
